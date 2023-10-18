2110b2a9946967ed15328ba2bb72cae1123d4035
Vinicius <viniciushm97@gmail.com>
2023-10-19 00:41:16 +0100

    Starting to implement Board and Piece

---
+++ untitled/src/Application/Program.java
@@ -0,0 +1,13 @@
+package Application;
+
+import Boardgame.Board;
+import Boardgame.Position;
+
+public class Program {
+    public static void main(String[] args) {
+        Board board = new Board(8,8);
+
+
+
+    }
+}
